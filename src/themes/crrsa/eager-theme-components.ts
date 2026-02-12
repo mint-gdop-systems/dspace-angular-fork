e3c62f0d379f43fad1d3f6188bc8d3d4410049ed
ashenfiDL <ashenafi.debella101@gmail.com>
2026-02-13 02:43:56 +0300

    feat: add file preview panel to workflow steps

--- src/themes/crrsa/eager-theme-components.ts
+++ src/themes/crrsa/eager-theme-components.ts
@@ -7,6 +7,12 @@ import { HomeNewsComponent } from "./app/home-page/home-news/home-news.component
 import { NavbarComponent } from "./app/navbar/navbar.component";
 import { AdminDashboardPageComponent } from "src/themes/crrsa/app/admin/admin-dashboard-page/admin-dashboard-page.component";
 import { UserDashboardComponent } from "src/themes/crrsa/app/admin/user-dashboard-page/user-dashboard-page.component";
+import { FilePreviewPanelComponent } from "src/themes/crrsa/app/shared/upload/file-preview-panel/file-preview-panel.component";
+import { UploaderComponent } from "src/themes/crrsa/app/shared/upload/uploader/uploader.component";
+import { SubmissionUploadFilesComponent } from "src/themes/crrsa/app/submission/form/submission-upload-files/submission-upload-files.component";
+import { SubmissionFormComponent } from "src/themes/crrsa/app/submission/form/submission-form.component";
+import { SubmissionSectionUploadFileComponent } from "src/themes/crrsa/app/submission/sections/upload/file/section-upload-file.component";
+import { SubmissionSectionUploadFileEditComponent } from "src/themes/crrsa/app/submission/sections/upload/file/edit/section-upload-file-edit.component";
 
 export const COMPONENTS = [
 	AdminSidebarComponent,
@@ -15,5 +21,11 @@ export const COMPONENTS = [
 	HeaderNavbarWrapperComponent,
 	NavbarComponent,
 	AdminDashboardPageComponent,
-	UserDashboardComponent
+	UserDashboardComponent,
+	FilePreviewPanelComponent,
+	UploaderComponent,
+	SubmissionUploadFilesComponent,
+	SubmissionFormComponent,
+	SubmissionSectionUploadFileComponent,
+	SubmissionSectionUploadFileEditComponent
 ];
