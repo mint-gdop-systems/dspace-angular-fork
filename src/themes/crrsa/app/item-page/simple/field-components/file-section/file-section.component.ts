f6cb5d0af6004c9b505f528dfed9c5e6298d1326
ashenfiDL <ashenafi.debella101@gmail.com>
2026-05-19 02:08:00 +0300

    Revert "feat: refactor file section and submission form layout, add preview panel wrapper"
    This reverts commit ab9fa2b6d63996fcb4760f0bcdc17e8d64b605b6.

--- src/themes/crrsa/app/item-page/simple/field-components/file-section/file-section.component.ts
+++ src/themes/crrsa/app/item-page/simple/field-components/file-section/file-section.component.ts
@@ -27,7 +27,9 @@ import {
 } from '@ngx-translate/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ThemedLoadingComponent } from 'src/app/shared/loading/themed-loading.component';
 import { MetadataFieldWrapperComponent } from 'src/app/shared/metadata-field-wrapper/metadata-field-wrapper.component';
+import { FileSizePipe } from 'src/app/shared/utils/file-size-pipe';
 import { VarDirective } from 'src/app/shared/utils/var.directive';
 import { FilePreviewPanelComponent } from 'src/themes/crrsa/app/shared/file-preview-panel/file-preview-panel.component';
 
@@ -40,7 +42,9 @@ import { FilePreviewPanelComponent } from 'src/themes/crrsa/app/shared/file-prev
   templateUrl: './file-section.component.html',
   imports: [
     CommonModule,
+    FileSizePipe,
     MetadataFieldWrapperComponent,
+    ThemedLoadingComponent,
     TranslateModule,
     VarDirective,
     FilePreviewPanelComponent,
